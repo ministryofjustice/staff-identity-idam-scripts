8e72705ced2ec039a44e7e8b82a1a242bbae2f76
John Nolan <john.nolan@justice.gov.uk>
2024-07-02 11:14:53 +0100

    Change to Select-Object

--- PerUserMfa/PerUserMfaEnable.ps1
+++ PerUserMfa/PerUserMfaEnable.ps1
@@ -6,10 +6,10 @@ $json = @{
 }
 
 # Check existing status
-Invoke-MgGraphRequest -Method GET -Uri "/beta/users/$userId/authentication/requirements" -OutputType PSObject | Select PerUserMFAState
+Invoke-MgGraphRequest -Method GET -Uri "/beta/users/$userId/authentication/requirements" -OutputType PSObject | Select-Object PerUserMFAState
 
 # Update MFA State
-Invoke-MgGraphRequest -Method PATCH -Uri "/beta/users/$userId/authentication/requirements" -OutputType PSObject -Body $json -ContentType "application/json" | Select PerUserMFAState
+Invoke-MgGraphRequest -Method PATCH -Uri "/beta/users/$userId/authentication/requirements" -OutputType PSObject -Body $json -ContentType "application/json" | Select-Object PerUserMFAState
 
 # Check new status
-Invoke-MgGraphRequest -Method GET -Uri "/beta/users/$userId/authentication/requirements" -OutputType PSObject | Select PerUserMFAState
+Invoke-MgGraphRequest -Method GET -Uri "/beta/users/$userId/authentication/requirements" -OutputType PSObject | Select-Object PerUserMFAState
