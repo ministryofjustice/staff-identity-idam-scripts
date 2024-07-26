06614eca86bd37e231657f1798f01c66ebe53fc2
John Nolan <john.nolan@justice.gov.uk>
2024-07-26 12:28:45 +0100

    Add Revoke Access script

---
+++ RevokeUserAccess/README.md
@@ -0,0 +1,39 @@
+# Revoke user access in Microsoft Entra ID
+
+Jira Ticket: [IDAM-861](https://dsdmoj.atlassian.net/browse/IDAM-861)
+Confluence Documentation: [TBC](#)
+
+## Description
+
+Scenarios that could require an administrator to revoke all access for a user include compromised accounts, employee termination, and other insider threats. Depending on the complexity of the environment, administrators can take several steps to ensure access is revoked. In some scenarios, there could be a period between the initiation of access revocation and when access is effectively revoked.
+
+For more information on this process, consult the following Microsoft Knowledge Article: [https://learn.microsoft.com/en-us/entra/identity/users/users-revoke-access#microsoft-entra-environment](https://learn.microsoft.com/en-us/entra/identity/users/users-revoke-access#microsoft-entra-environment).
+
+The script within this directory allow you to do the following.
+
+| Name | Description | Link |
+|------|-------------|------|
+| Revoke user access from Entra ID. | Disables Entra account, revokes refresh tokens and disables user's device. | [Link](./Revoke.ps1) |
+
+## Prerequisites
+
+- Relevant PIM roles available
+    - `Global Reader`
+    - `Application Administrator`
+- PowerShell 7
+- Administrator rights to run PowerShell
+
+## Executing Script
+
+* Download and save a copy of this script.
+* PIM the following roles
+    - `Global Reader`
+    - `Application Administrator`
+* Open PowerShell 7 and run the following commands
+    * If this is your first run `Install-Module Microsoft.Graph.Beta`
+    * If this is already installed, run `Update-Module Microsoft.Graph.Beta`
+* `cd` to where you downloaded the local script
+* Run the required script
+* At the prompt, sign in with your Entra ID Administrator Account
+
+The script should now run successfully. Follow the onscreen prompts.
---
+++ RevokeUserAccess/Revoke.ps1
@@ -0,0 +1,81 @@
+Connect-MgGraph -Scopes "User.ReadWrite.All" -NoWelcome
+Import-Module Microsoft.Graph.Beta.Applications
+
+Write-Host "Welcome to the Revoke user access in Microsoft Entra ID PowerShell script.`n" -ForegroundColor Green
+Write-Host "For more information on this process, consult the following Microsoft Knowledge Article: https://learn.microsoft.com/en-us/entra/identity/users/users-revoke-access#microsoft-entra-environment.`n" -ForegroundColor Green
+Write-Host "Warning: Running this script will disable a users account and device. Please ensure you understand the process fully before going any further.`n" -ForegroundColor Red
+
+$UserUPN = Read-Host -Prompt "Enter users principle name to be revoked"
+
+if (!$UserUPN) {
+    Write-Error "UPN empty."
+    return
+}
+
+$user = Get-MgBetaUser -Search UserPrincipalName:$UserUPN -ConsistencyLevel eventual | Format-List  ID, DisplayName, Mail, UserPrincipalName
+
+if ($user.Count -eq 0) {
+    Write-Error "User $UserUPN not found."
+    return
+}
+
+if ($user.Count -ne 5) {
+    Write-Error "Multiple users found for search: $UserUPN."
+    return
+}
+
+Write-Host "`n$UserUPN details" -ForegroundColor Blue
+$user
+
+$ConfirmCorrectUser = Read-Host -Prompt "Are the above details correct? If so, please re-enter the users principle name to continue"
+
+if ($UserUPN -ne $ConfirmCorrectUser) {
+    Write-Error "UPN values do not match. Process cancelled."
+    return
+}
+
+# Step 1
+Write-Host "`nStep 1 - Disable users account in Entra." -ForegroundColor Blue
+
+$ConfirmStep1 = Read-Host -Prompt "Enter Y to continue"
+
+if ($ConfirmStep1.ToLower() -ne "y") {
+    Write-Error "Process cancelled at Step 1."
+    return
+}
+
+# Update-MgUser -UserId $user.Id -AccountEnabled:$false
+
+Write-Host "Step 1 Complete. $UserUPN disabled." -ForegroundColor Green
+
+# Step 2
+Write-Host "`nStep 2 - Revoke the user's Microsoft Entra ID refresh tokens." -ForegroundColor Blue
+
+$ConfirmStep1 = Read-Host -Prompt "Enter Y to continue"
+
+if ($ConfirmStep1.ToLower() -ne "y") {
+    Write-Error "Process cancelled at Step 2."
+    return
+}
+
+# Revoke-MgUserSignInSession -UserId $user.Id
+
+Write-Host "Step 2 Complete. $UserUPN refresh tokens revoked." -ForegroundColor Green
+
+# Step 3
+Write-Host "`nStep 3 - Disable the user's devices." -ForegroundColor Blue
+
+$ConfirmStep1 = Read-Host -Prompt "Enter Y to continue"
+
+if ($ConfirmStep1.ToLower() -ne "y") {
+    Write-Error "Process cancelled at Step 3."
+    return
+}
+
+# $Device = Get-MgUserRegisteredDevice -UserId $User.Id 
+# Update-MgDevice -DeviceId $Device.Id -AccountEnabled:$false
+
+Write-Host "Step 3 Complete. $UserUPN devices disabled." -ForegroundColor Green
+
+# Process completed
+Write-Host "`nEntra User successfully disabled." -ForegroundColor Green
