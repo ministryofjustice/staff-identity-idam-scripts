7c5867e530010c79e1b564d930a29cf98921a7ee
John Nolan <john.nolan@justice.gov.uk>
2025-01-08 09:13:15 +0000

    Gets last login date and licence for users (#10)
    * Gets last login date and licence for users

--- PrisonIdentities/PrisonIdentitiesGetAll.ps1
+++ PrisonIdentities/PrisonIdentitiesGetAll.ps1
@@ -1,8 +1,9 @@
 #Connet to Microsoft Graph
-Connect-MgGraph -Scope "User.Read.All, Application.Read.All,  User.ReadWrite.All, Directory.Read.All, Application.ReadWrite.All, Policy.ReadWrite.AuthenticationMethod"
+Connect-MgGraph -Scope "User.Read.All, Application.Read.All,  User.ReadWrite.All, Directory.Read.All, Application.ReadWrite.All, Policy.ReadWrite.AuthenticationMethod, AuditLog.Read.All"
 
 #Get all users and select only required properties
-$allUsers = Get-MgUser -All -Property UserPrincipalName, GivenName, Surname, Mail, OfficeLocation, AccountEnabled | Select-Object UserPrincipalName, GivenName, Surname, Mail, OfficeLocation, AccountEnabled
+#$allUsers = Get-MgUser -Top 2000 -Property UserPrincipalName, GivenName, Surname, Mail, OfficeLocation, AccountEnabled, SignInActivity | Select-Object UserPrincipalName, GivenName, Surname, Mail, OfficeLocation, AccountEnabled, SignInActivity
+$allUsers = Get-MgUser -All -Property UserPrincipalName, GivenName, Surname, Mail, OfficeLocation, AccountEnabled, SignInActivity | Select-Object UserPrincipalName, GivenName, Surname, Mail, OfficeLocation, AccountEnabled, SignInActivity
 
 $InputArray = @(
     "HMP Altcourse",
@@ -21,12 +22,29 @@ $InputArray = @(
     "HMP Thameside"
 )
 
-#initialise array
 $AllUsersPrisonIdentities = [System.Collections.Generic.List[Object]]::new()
 
-#Loop through each user and add results to array
-Foreach ($user in $allusers){
+Foreach ($user in $allusers) {
     if ($InputArray -contains $user.OfficeLocation) {
+
+        $LastLoginDate = $user.SignInActivity.LastSignInDateTime
+        $user | Add-Member -MemberType NoteProperty -Name LastLoginDate -Value $LastLoginDate -Force
+        $user.PSObject.Properties.Remove('SignInActivity')
+        
+        $LicenceList = ""
+        $LicensesData = Get-MgUserLicenseDetail -userid $user.UserPrincipalName
+        foreach ($LicenceDataEntry in $LicensesData) {
+            $LicenseSKUs = $LicenceDataEntry.SkuPartNumber
+            
+            foreach ($License in $LicenseSKUs) {
+                if ($License -eq "M365_E5_SUITE_COMPONENTS") {
+                    $LicenceList = $License
+                }
+            }
+        }
+
+        $user | Add-Member -Name Licences -Value $LicenceList -MemberType NoteProperty
+        
         $AllUsersPrisonIdentities.Add($user)
     }
 }
