3fa1c6e4796f6eb3d9b345ab7999c4254268c19b
MikeT-MoJ <michael.tulett@justice.gov.uk>
2025-02-27 15:29:13 +0000

    Update BatchDeactivateUsers/BatchDeactivateUsers.ps1
    Co-authored-by: jasegillettmoj <jason.gillett@justice.gov.uk>

--- BatchDeactivateUsers/BatchDeactivateUsers.ps1
+++ BatchDeactivateUsers/BatchDeactivateUsers.ps1
@@ -89,7 +89,6 @@ if ($check -ne "YES"){
 write-host "proceeding in 10 seconds... use ctrl+c to abort" -ForegroundColor Cyan
 Start-Sleep -Seconds 10
 
-
 foreach ($user in $users){
     write-host "reverting $user.Name ..." -ForegroundColor Cyan
     set-aduser $user.UserPrincipalName.Split("@")[0] -Description $user.Description -Enabled $true
